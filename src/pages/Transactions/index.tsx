e6e0b17b277464a9042e7f5d0168a23ddd4ac13c
Daniel Azoulay <danielazoulay2010@gmail.com>
2025-02-05 18:45:45 -0300

    feat(src/pages/transactions): create table of transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,38 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import {
+	PriceHighlight,
+	TransactionsContainer,
+	TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+	return (
+		<div>
+			<Header />
+			<Summary />
+			<TransactionsContainer>
+				<SearchForm />
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width="50%">Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+							</td>
+							<td>13/04/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Hambúrguer</td>
+							<td>
+								<PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
+							</td>
+							<td>13/04/2023</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
+		</div>
+	);
 }
