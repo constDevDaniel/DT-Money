1a1c4c88ae4e5801151b75968bfc5bd73a945f18
Daniel Azoulay <danielazoulay2010@gmail.com>
2025-02-11 18:37:30 -0300

    feat(index.tsx summary): formatter currency values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,11 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { useSummary } from "../../hooks/useSummary";
+import { currencyFormatter } from "../../utils/formatter";
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
+	const summary = useSummary();
+
 	return (
 		<SummaryContainer>
 			<SummaryCard>
@@ -9,21 +13,21 @@ export function Summary() {
 					<span>Entradas</span>
 					<ArrowCircleUp size={32} color="#00b37e" />
 				</header>
-				<strong>R$ 71.300,00</strong>
+				<strong>{currencyFormatter(summary.income)}</strong>
 			</SummaryCard>
 			<SummaryCard>
 				<header>
 					<span>Saídas</span>
 					<ArrowCircleDown size={32} color="#f75a68" />
 				</header>
-				<strong>R$ 1.300,00</strong>
+				<strong>{currencyFormatter(summary.outcome)}</strong>
 			</SummaryCard>
 			<SummaryCard variant="green">
 				<header>
 					<span>Total</span>
 					<CurrencyDollar size={32} color="#fff" />
 				</header>
-				<strong>R$ 71.300,00</strong>
+				<strong>{currencyFormatter(summary.total)}</strong>
 			</SummaryCard>
 		</SummaryContainer>
 	);
