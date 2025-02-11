9eae1a32996a1cfc222d9944f840dfef7a4ff634
Daniel Azoulay <danielazoulay2010@gmail.com>
2025-02-11 18:44:23 -0300

    feat(index.tsx transactions): add context and list all transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,8 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { currencyFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import {
 	PriceHighlight,
@@ -8,6 +11,8 @@ import {
 } from "./styles";
 
 export function Transactions() {
+	const { transactions } = useContext(TransactionsContext);
+
 	return (
 		<div>
 			<Header />
@@ -16,20 +21,22 @@ export function Transactions() {
 				<SearchForm />
 				<TransactionsTable>
 					<tbody>
-						<tr>
-							<td width="50%">Desenvolvimento de site</td>
-							<td>
-								<PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-							</td>
-							<td>13/04/2023</td>
-						</tr>
-						<tr>
-							<td width="50%">Hambúrguer</td>
-							<td>
-								<PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight>
-							</td>
-							<td>13/04/2023</td>
-						</tr>
+						{transactions.map((transactions) => {
+							return (
+								<tr key={transactions.id}>
+									<td width="50%">{transactions.description}</td>
+									<td>
+										<PriceHighlight variant={transactions.type}>
+											{transactions.type === "outcome" && "- "}
+											{currencyFormatter(transactions.price)}
+										</PriceHighlight>
+									</td>
+									<td>
+										{new Date(transactions.createdAt).toLocaleDateString()}
+									</td>
+								</tr>
+							);
+						})}
 					</tbody>
 				</TransactionsTable>
 			</TransactionsContainer>
